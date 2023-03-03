426a1510bc6844c952e83cd7b23321f4b65fcd19
Rutuja Lomate <104217802+Rutujalomate@users.noreply.github.com>
2023-03-03 13:46:41 +0530

    Add files via upload

--- src/config/db.js
+++ src/config/db.js
@@ -1,7 +1,7 @@
 const mongoose=require("mongoose")
 
 const main=async()=>{
-    return mongoose.connect("mongodb://127.0.0.1:27017/blogusers")
+    return mongoose.connect("mongodb+srv://r:lo@cluster0.3507kfr.mongodb.net/?retryWrites=true&w=majority")
     .then(()=>console.log('connected to database'))
     .catch((e)=>console.log(e))
    // console.log("conneted")
