cd7f404af393bf856e5efb828d9453d77f2a98a2
Rutuja Lomate <104217802+Rutujalomate@users.noreply.github.com>
2023-03-03 14:02:58 +0530

    Add files via upload

--- src/features/user/user.router.js
+++ src/features/user/user.router.js
@@ -13,7 +13,7 @@ const blacklist=[]
 
 
 app.post("/signup",async(req,res)=>{
-   // const {name,email,password,age}=req.body
+    const {name,email,password,age}=req.body
 // const token=req.headers["token"]
 //     try{
 //         if(token){
@@ -39,7 +39,11 @@ app.post("/signup",async(req,res)=>{
    // let user=new User({name,email,password,age})
    // console.log('user2',user)
    // await user.save()
-  return  res.status(201).send('user created sucessfully')
+   let user=await User.create({
+    name,email,password,age
+})
+console.log("user",user)
+  return  res.status(201).send(user)
 
 })
 
